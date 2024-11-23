20c15c68cea41659c2875648415d8e53dcbd3f41
Omigaass <miguelanperibeiro@gmail.com>
2024-11-23 01:41:10 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/public', express.static(path.join(__dirname, './public')));
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, './pages/home.html'));
+});
+
+
+module.exports = app;
+
+if (require.main === module) {
+  const PORT = 3000;
+  app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + './pages/index.html');
+});
+
+// ------------------------------ //
+
+const usuarioRoute = require('../backend/src/routes/usuarioRoute');
+app.use('/api/auth', usuarioRoute);
+
+const movimentacaoRoute = require('../backend/src/routes/movimentacaoRoute');
+app.use('/api/movimentacao', movimentacaoRoute);
