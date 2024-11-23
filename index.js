609f4c194fc9286ca942fdab63340d7087c3ce6a
Omigaass <miguelanperibeiro@gmail.com>
2024-11-22 23:49:38 -0300

    por favor, socorro

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 
-app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/public', express.static(path.join(__dirname, 'web/src/public')));
-// Rota para a página Home
+
 app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, 'web/src/pages/home.html'));
 });
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
+module.exports = app;
+
+if (require.main === module) {
+  const PORT = 3000;
+  app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+}
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/web/src/pages/index.html');
